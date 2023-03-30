4a8621aa9233049ccb513d4a52923b21f01f9796
asadparkar <58441390+asadparkar@users.noreply.github.com>
2023-03-30 16:30:34 +0530

    feat: Added Navigation Bar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import Navbar from './components/navbar'
 
 function App() {
 
   return (
     <div className="App">
+    <Navbar />
       <h1 className=''>LOL</h1>
     </div>
   )
