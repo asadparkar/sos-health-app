091714acc7fec970718576c3cfdec8c133cb21a2
asadparkar <58441390+asadparkar@users.noreply.github.com>
2023-04-02 06:20:42 +0530

    feat: Fixed React Router Bugs

--- hospital/src/App.jsx
+++ hospital/src/App.jsx
@@ -4,7 +4,7 @@ import Navbar from './components/navbar'
 import Footer from './components/footer'
 import Home from './pages/home'
 import { Route, Routes } from 'react-router-dom'
-
+import Ambulance from './pages/ambulance'
 function App() {
 
   return (
@@ -12,7 +12,8 @@ function App() {
     <Navbar />
      <div className='sm:p-10 p-2' style={{minHeight:'85vh'}}>
      <Routes>
-      <Route path='/' component={<Home />} />
+      <Route path='/' element={<Home />} />
+      <Route path='/ambulance' element={<Ambulance />} />
      </Routes>
     </div>
     <footer>
---
+++ hospital/src/pages/ambulance.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Ambulance = () => {
+  return (
+    <div>Ambulance</div>
+  )
+}
+
+export default Ambulance
