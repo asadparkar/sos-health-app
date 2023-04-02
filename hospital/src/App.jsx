e0a27074aacee1f048c878f0a4fae1a7de0a81f4
umam <infernape_999@hotmail.com>
2023-04-02 17:18:19 +0530

    feat : implemented create acc with email and password, sign in , log out functionality, and authorised access page when logged in only

--- hospital/src/App.jsx
+++ hospital/src/App.jsx
@@ -5,16 +5,29 @@ import Footer from './components/footer'
 import Home from './pages/home'
 import { Route, Routes } from 'react-router-dom'
 import Ambulance from './pages/ambulance'
+import Signin from './components/Signin';
+import Signup from './components/Signup';
+import Account from './components/Account';
+import { AuthContextProvider } from './context/AuthContext';
+import ProtectedRoute from './components/ProtectedRoute'
 function App() {
 
   return (
     <div className="App">
     <Navbar />
      <div className='sm:p-10 p-2' style={{minHeight:'85vh'}}>
+     <AuthContextProvider>
      <Routes>
-      <Route path='/' element={<Home />} />
+      <Route path='/' element={<Signin />} />
+      <Route path='/signup' element={<Signup />} />
+      <Route path='/account' element={
+        <ProtectedRoute>
+          <Account />
+        </ProtectedRoute>
+      } />
       <Route path='/ambulance' element={<Ambulance />} />
      </Routes>
+     </AuthContextProvider>
     </div>
     <footer>
       <Footer />
