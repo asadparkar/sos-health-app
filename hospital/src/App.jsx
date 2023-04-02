8f092aaac436a0e82863e623039a3865e99f0a6a
asadparkar <58441390+asadparkar@users.noreply.github.com>
2023-04-02 05:48:52 +0530

    feat: Implemented Navbar and Footer for Hospital

--- hospital/src/App.jsx
+++ hospital/src/App.jsx
@@ -1,11 +1,23 @@
 import { useState } from 'react'
 import './App.css'
+import Navbar from './components/navbar'
+import Footer from './components/footer'
+import Home from './pages/home'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
 
   return (
     <div className="App">
-
+    <Navbar />
+     <div className='sm:p-10 p-2' style={{minHeight:'85vh'}}>
+     <Routes>
+      <Route path='/' component={<Home />} />
+     </Routes>
+    </div>
+    <footer>
+      <Footer />
+    </footer>
     </div>
   )
 }
