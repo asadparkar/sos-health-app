af9cc8e293f43f8a6ac8f6a2e0521692b3cefbb8
asadparkar <58441390+asadparkar@users.noreply.github.com>
2023-04-15 18:57:07 +0530

    fix: Fixing bugs

--- client/src/pages/sos.jsx
+++ client/src/pages/sos.jsx
@@ -61,7 +61,7 @@ setAmbList(filteredData)
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[19.152896, 73.0365952]}>
+        <Marker position={[latitude, longitude]}>
             <Popup>
             A pretty CSS3 popup. <br /> Easily customizable.
             </Popup>
