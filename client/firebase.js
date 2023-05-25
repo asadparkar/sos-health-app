59834cf9f3d8ba0fe448e4a45177d4185c93ad93
umam <infernape_999@hotmail.com>
2023-05-25 22:28:26 +0530

    feat : firebase db restored

--- client/firebase.js
+++ client/firebase.js
@@ -24,15 +24,27 @@ import {getFirestore} from "firebase/firestore"
 // };
 
 //third db
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBr9f875eiFqiGXIv8gpNJVLV1nNm6Jm0U",
+//   authDomain: "thirdbase-8b28d.firebaseapp.com",
+//   projectId: "thirdbase-8b28d",
+//   storageBucket: "thirdbase-8b28d.appspot.com",
+//   messagingSenderId: "703512896862",
+//   appId: "1:703512896862:web:df0ac0ffdcb07d15b9dbcf",
+//   measurementId: "G-2CV0DSXZCZ"
+// };
+
+//fourth db
 const firebaseConfig = {
-  apiKey: "AIzaSyBr9f875eiFqiGXIv8gpNJVLV1nNm6Jm0U",
-  authDomain: "thirdbase-8b28d.firebaseapp.com",
-  projectId: "thirdbase-8b28d",
-  storageBucket: "thirdbase-8b28d.appspot.com",
-  messagingSenderId: "703512896862",
-  appId: "1:703512896862:web:df0ac0ffdcb07d15b9dbcf",
-  measurementId: "G-2CV0DSXZCZ"
+  apiKey: "AIzaSyCF66H7Njkx_8cW3WXvZ_8_9hVNzqsF4sE",
+  authDomain: "fourthfirebase-eb046.firebaseapp.com",
+  projectId: "fourthfirebase-eb046",
+  storageBucket: "fourthfirebase-eb046.appspot.com",
+  messagingSenderId: "108379634149",
+  appId: "1:108379634149:web:b919e46c3b64cebf8d170c",
+  measurementId: "G-TWWE0H0436"
 };
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
